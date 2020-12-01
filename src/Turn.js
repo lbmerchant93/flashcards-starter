47844eae10682a59d9a901642a6c3c297a3e0ca0
Lucas Merchant <lbmerchant93@gmail.com>
2020-11-30 18:29:14 -0700

    Create test for evaluateGuess and giveFeedback methods, then create methods to pass test

--- src/Turn.js
+++ src/Turn.js
@@ -2,14 +2,28 @@ class Turn {
   constructor(guess, card) {
     this.guess = guess;
     this.card = card;
-  }
+    this.guessEvaluation;
+  };
   returnGuess() {
     return this.guess;
-  }
+  };
   returnCard() {
     return this.card;
-  }
-}
-
+  };
+  evaluateGuess() {
+    if(this.guess === this.card.correctAnswer) {
+      return this.guessEvaluation = true;
+    } else {
+      return this.guessEvaluation = false;
+    };
+  };
+  giveFeedback() {
+    if(this.guessEvaluation === true) {
+      return 'correct!'
+    } else {
+      return 'incorrect!'
+    };
+  };
+};
 
 module.exports = Turn;
--- test/Turn-test.js
+++ test/Turn-test.js
@@ -8,23 +8,27 @@ describe('Turn', function() {
 
   it('should be a function', function() {
     const turn = new Turn();
+
     expect(Turn).to.be.a('function');
   });
 
   it('should be an instance of Turn', function() {
     const turn = new Turn();
+
     expect(turn).to.be.an.instanceof(Turn);
   });
 
   it('should contain a method that will return a user\'s guess', function() {
     const card = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
     const turn = new Turn('pug', card);
+
     expect(turn.returnGuess()).to.deep.equal('pug');
   });
 
   it('should contain a method that will return a Card object for the current card in play', function() {
     const card = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
     const turn = new Turn('pug', card);
+
     expect(turn.returnCard()).to.deep.equal({
       id: 1,
       question: 'What is Robbie\'s favorite animal',
@@ -33,13 +37,19 @@ describe('Turn', function() {
     });
   });
 
+  it('should contain a method that will compare the guess to correctAnswer and return whether they match or not', function() {
+    const card = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    const turn = new Turn('pug', card);
 
+    expect(turn.evaluateGuess()).to.deep.equal(false);
+  });
 
+  it('should contain a method that will return either \'incorrect!\' or \'correct!\' based on whether the guess is correct or not', function() {
+    const card = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    const turn = new Turn('pug', card);
 
+    turn.evaluateGuess();
 
-
-
-
-
-
+    expect(turn.giveFeedback()).to.deep.equal('incorrect!');
+  });
 });
