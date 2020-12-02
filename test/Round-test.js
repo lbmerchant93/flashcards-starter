10b057332132ac2deaa074562692ab601e27ee6c
Lucas Merchant <lbmerchant93@gmail.com>
2020-12-01 19:03:07 -0700

    Create test for endRound method to print message with percent correct

--- test/Round-test.js
+++ test/Round-test.js
@@ -148,4 +148,23 @@ describe('Round', function() {
     expect(round.percentageCorrect).to.equal(66);
   });
 
+  it('should contain a method that prints \'**Round over!** You answered <>% of the questions correctly!\'', function() {
+    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    const card3 = new Card(12, 'What is Travis\'s favorite stress reliever?', ['listening to music', 'watching Netflix', 'playing with bubble wrap'], 'playing with bubble wrap');
+
+    const deck = new Deck([card1, card2, card3]);
+    const round = new Round(deck);
+
+    round.returnCurrentCard();
+    round.takeTurn('sea otter');
+    round.returnCurrentCard();
+    round.takeTurn('spleen');
+    round.returnCurrentCard();
+    round.takeTurn('playing with bubble wrap');
+    round.calculatePercentCorrect();
+
+    expect(round.endRound()).to.equal('**Round over!** You answered 66% of the questions correctly!');
+  });
+
 });
