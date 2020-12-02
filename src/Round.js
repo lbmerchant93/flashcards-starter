b5678cb3acf302477db5d025bca37fa9514d9983
Lucas Merchant <lbmerchant93@gmail.com>
2020-12-01 19:07:13 -0700

    Pass test for endRound method

--- src/Round.js
+++ src/Round.js
@@ -21,6 +21,11 @@ class Round {
   calculatePercentCorrect() {
     this.percentageCorrect = Math.floor(((this.turns - this.incorrectGuesses.length) /this.turns) * 100);
   };
+  endRound()
+//will need to delete this comment and either the console.log or the return below depending on what is needed later 
+    console.log(`**Round over!** You answered ${this.percentageCorrect}% of the questions correctly!`);
+    return `**Round over!** You answered ${this.percentageCorrect}% of the questions correctly!`
+  };
 };
 
 module.exports = Round;
