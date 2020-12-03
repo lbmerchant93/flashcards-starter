26a918f60a1e31fdc96e79883db674ae01ed0915
Lucas Merchant <lbmerchant93@gmail.com>
2020-12-02 17:00:54 -0700

    Edit currentRound property, create Cards, puts Cards in Deck, create Round using Deck

--- src/Game.js
+++ src/Game.js
@@ -1,10 +1,21 @@
 const data = require('./data');
 const prototypeQuestions = data.prototypeData;
 const util = require('./util');
+const Card = require('../src/Card');
+const Deck = require('../src/Deck');
+const Round = require('../src/Round');
 
 class Game {
-  constructor(round) {
-    this.currentRound = round;
+  constructor() {
+    this.currentRound;
+  };
+  start() {
+    const cards = prototypeQuestions.map(gameCard => {
+    const card = new Card(gameCard.id, gameCard.question, gameCard.answers, gameCard.correctAnswer);
+    return card;
+  });
+    const deck = new Deck(cards);
+    this.currentRound = new Round(deck);
   };
   printMessage(deck, round) {
       console.log(`Welcome to FlashCards! You are playing with ${deck.countCards()} cards.
--- test/Game-test.js
+++ test/Game-test.js
@@ -1,24 +1,16 @@
 const chai = require('chai');
 const expect = chai.expect;
 
-const Card = require('../src/Card');
-const Deck = require('../src/Deck');
+// const Card = require('../src/Card');
+// const Deck = require('../src/Deck');
 const Round = require('../src/Round');
 const Game = require('../src/Game');
 
 describe('Game', () => {
-  let game, round, deck, card1, card2;
-
-  beforeEach(() => {
-    card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
-    card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
-    deck = new Deck([card1, card2]);
-    round1 = new Round(deck);
-    game = new Game(round1);
-  });
 
   it('should keep track of the currentRound', () => {
-    console.log(game.currentRound);
-    expect(game.currentRound).to.equal(round1);
+    const game = new Game();
+    game.start();
+    expect(game.currentRound).to.be.an.instanceof(Round);
   });
 });
