9a4181512c7101dea87bcd961723cbf8785bb00c
Lucas Merchant <lbmerchant93@gmail.com>
2020-12-03 14:53:07 -0700

    Add to endRound message on how to exit game

--- src/Round.js
+++ src/Round.js
@@ -20,7 +20,7 @@ class Round {
     return Math.floor(((this.turns - this.incorrectGuesses.length) / this.turns) * 100);
   }
   endRound() {
-    console.log(`**Round over!** You answered ${this.calculatePercentCorrect()}% of the questions correctly!`);
+    console.log(`**Round over!** You answered ${this.calculatePercentCorrect()}% of the questions correctly! Press control + c to exit game.`);
   }
 }
 
